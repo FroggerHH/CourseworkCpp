8aa7e550dfa7431bafc60e9e06084d82e24921b7
Vyacheslav <imjustafrogger@gmail.com>
2025-07-13 15:13:42 +0300

    оптимизация num1 под блок-схему

--- num1.cpp
+++ num1.cpp
@@ -4,16 +4,20 @@
 class Graph {
 private:
     int numVertices;
-    std::vector<std::vector<int> > adj; // Список смежности
+    std::vector<std::vector<int>> adj; // Список смежности
     std::vector<bool> visited;
     int visitedCount;
 
     void dfs(int v) {
         visited[v] = true;
         visitedCount++;
-        for (int neighbor: adj[v])
-            if (!visited[neighbor])
-                dfs(neighbor);
+        
+        int size = adj[v].size();
+        for (int i = 0; i < size; i++) {
+            int neighbor = adj[v][i];
+            bool isVisited = visited[neighbor];
+            if (!isVisited) dfs(neighbor);
+        }
     }
 
 public:
@@ -23,56 +27,41 @@ public:
         if (u >= 0 && u < numVertices && v >= 0 && v < numVertices) {
             adj[u].push_back(v);
             adj[v].push_back(u); // Добавляем обратное ребро для неориентированного графа
-        } else
-            std::cerr << "Ошибка: некорректные индексы вершин (" << u << ", " << v << ")." << std::endl;
+        }
     }
 
     bool isConnected() {
-        if (numVertices <= 1) return true;
-
-        // Сброс состояния для нового DFS обхода
-        visited.assign(numVertices, false);
-        visitedCount = 0;
-
-        dfs(0);
-
-        return visitedCount == numVertices;
+        if (numVertices >= 1) {
+          // Сброс состояния для нового DFS обхода
+          visited.assign(numVertices, false);
+          visitedCount = 0;
+          dfs(0);
+          return visitedCount == numVertices;
+        } else return true;
     }
 };
 
 int main() {
-#ifdef _WIN32
     std::setlocale(LC_ALL, "ru_RU.UTF-8");
-    system("chcp 65001 > nul");
-    system("cls");
-#endif
 
     int N, M; // N - количество вершин, M - количество ребер
 
     std::cout << "Введите количество вершин графа: ";
     std::cin >> N;
-    if (N < 0) {
-        std::cerr << "Ошибка: количество вершин не может быть отрицательным." << std::endl;
-        return 1;
-    }
-
-    Graph graph(N);
-
     std::cout << "Введите количество ребер графа: ";
     std::cin >> M;
-    if (M < 0) {
-        std::cerr << "Ошибка: количество ребер не может быть отрицательным." << std::endl;
-        return 1;
-    }
+    
+    Graph graph(N);
 
     std::cout << "Введите " << M << " пар вершин (u v) для каждого ребра (вершины нумеруются с 0 до " << N - 1 << "):" << std::endl;
-    for (int i = 0; i < M; ++i) {
+    for (int i = 0; i < M; i++) {
         int u, v;
         std::cin >> u >> v;
         graph.addEdge(u, v);
     }
 
-    if (graph.isConnected()) std::cout << "Граф является связным." << std::endl;
+    bool isConnected = graph.isConnected();
+    if (isConnected) std::cout << "Граф является связным." << std::endl;
     else std::cout << "Граф не является связным." << std::endl;
 
     return 0;
